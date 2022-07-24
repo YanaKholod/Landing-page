07b78c43a19f046419809c748a46a5cffe125918
developer <developer@MacBook-Pro-developer.local>
2022-07-24 14:36:31 +0300

    sixth commit

--- src/components/Courses/Course.jsx
+++ src/components/Courses/Course.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { state } from "../store";
-
+import s from "./Course.module.css";
 const Course = () => {
   const { id } = useParams();
   const post = state[id];
 
   return (
-    <div>
-      <p>{post.title}</p>
-      <div>{post.content}</div>
+    <div className={s.allCourse}>
+      <h1>{post.title}</h1>
+      <div className={s.content}>{post.content}</div>
+      <div className={s.description}>{post.description}</div>
     </div>
   );
 };
